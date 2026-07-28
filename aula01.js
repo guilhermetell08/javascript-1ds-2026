6f73a3eafd9515c608dd473b837b99e1bc7d0453
Guilherme Maidl <guilhermemaidl5310@gmail.com>
2026-07-28 11:20:34 -0300

    Add files via upload

---
+++ aula01.js
@@ -0,0 +1,10 @@
+// CRIANDO VARIÁVEIS PARA O SISTEMA
+
+let nome = "Guilherme";
+var idade = 16; // Não utilize embora esteja no javascript
+const altura = 1.71;
+
+console.log( nome );
+console.log( idade );
+console.log( altura );
+
