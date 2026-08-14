1007a02087adcb4c7204f3b43a2a8785f0ff11fb
Guilherme Maidl <guilhermemaidl5310@gmail.com>
2026-08-14 08:37:40 -0300

    Add files via upload

---
+++ aula03.js
@@ -0,0 +1,19 @@
+for(let i =0; i < 10; i++){
+    console.log("Loop de repetição de 10 vezes!");
+}
+
+//USANDO LAÇO DE REPETIÇÃO FOR PARA RECORRER UM VETOR
+const frases = [
+    "Preparando motores",
+    "Checando sistemas",
+    "Autorização recebida",
+    "Contagem regressiva",
+    "ignição"
+];
+
+//VETOR = [ 0, 1, 2, 3, 4 ]
+console.log( frases[2] );
+console.log( frases[4] );
+
+//
+console.log( frases[3] );
